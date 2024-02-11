d10262f1779fae3a0cbd61ce031c37dad3d7d528
adhyasbhat <adhyasbhat@gmail.com>
2024-02-11 21:36:51 +0530

    sec

--- script.js
+++ script.js
@@ -5,36 +5,41 @@ const w2 = document.querySelector('.w-2');
 let counter = 0
 yes.addEventListener('click', () => {
     w1.style.display = 'none';
-    w2.style.display = 'block';
+    w2.style.display = 'flex';
 });
 
 no.addEventListener('click', () => {    
  counter++;
  if(counter == 1){
-    no.innerHTML = 'Are you sure?';
-    yes.style.height = '25%';
-    yes.style.width = '25%';
+    no.innerHTML = 'No (Are you sure?)';
+    yes.style.height = '7rem';
+    yes.style.width = '11rem';
+    no.style.height = '4rem';
+    no.style.width = '8rem';
+    no.style.fontSize = '1rem'
+
  }
  else if(counter == 2){
-    no.innerHTML = 'Really?';
-    yes.style.height = '50%';
-    yes.style.width = '50%';
+    no.innerHTML = 'No (Really?)';
+    yes.style.height = '8rem';
+    yes.style.width = '12rem';
  }
  else if(counter == 3){
-    no.innerHTML = 'Think again!';
-    yes.style.height = '75%';
-    yes.style.width = '75%';
+    no.innerHTML = 'No (Think again!)';
+    yes.style.height = '9rem';
+    yes.style.width = '12rem';
  }
  else if(counter == 4){
     no.innerHTML = 'Definitely Yes';
-    yes.style.height = '50%';
-    yes.style.width = '50%';
-    no.style.height = '50%';
-    no.style.width = '50%';
+    yes.style.height = '9rem';
+    yes.style.width = '12rem';
+    no.style.height = '9rem';
+    no.style.width = '12rem';
+    no.style.backgroundColor = "#28a745"
  }
  else if(counter >= 5){
     w1.style.display = 'none';
-    w2.style.display = 'block';
+    w2.style.display = 'flex';
  }
 
 });
